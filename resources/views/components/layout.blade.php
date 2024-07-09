be714a29d48b76a74783478ec4e3febdf0af33b9
Joey Kudish <joachim.kudish@gmail.com>
2024-07-08 19:06:14 -0700

    layout

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Joey Kudish</title>
+    <link rel="preload" href="{{ url('fonts/Telegraf UltraBold 800.woff') }}" as="font" crossorigin/>
+    <link rel="preload" href="{{ url('fonts/muli-regular-webfont.woff2') }}" as="font" crossorigin/>
+    <link rel="preload" href="{{ url('fonts/muli-regular-webfont.woff') }}" as="font" crossorigin/>
+    @vite('resources/css/app.css')
+</head>
+<body class="dark:bg-zinc-800">
+    {{ $slot }}
+</body>
+</html>
