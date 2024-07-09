be1b7ce3033edb440038d802feba0c4411633aa6
Joey Kudish <joachim.kudish@gmail.com>
2024-07-08 19:01:12 -0700

    remove grid preload doesn’t seem to work

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,6 @@
     <link rel="preload" href="{{ url('fonts/Telegraf UltraBold 800.woff') }}" as="font" crossorigin/>
     <link rel="preload" href="{{ url('fonts/muli-regular-webfont.woff2') }}" as="font" crossorigin/>
     <link rel="preload" href="{{ url('fonts/muli-regular-webfont.woff') }}" as="font" crossorigin/>
-    <link rel="preload" href="{{ url('img/grid.svg') }}" as="image" crossorigin/>
     @vite('resources/css/app.css')
 </head>
 <body class="dark:bg-zinc-800">
