065a22f68b0200ba83911182a358670c6f6e70b1
Joey Kudish <joachim.kudish@gmail.com>
2024-07-09 21:24:58 -0700

    route tweaks

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,18 @@ use Illuminate\Support\Facades\Route;
 
 // redirect slides subdomain to speaking
 Route::domain('slides.jkudish.com')->group(function () {
+    Route::redirect('/', 'https://jkudish.com/speaking');
     Route::fallback(fn () => redirect('/speaking'));
 });
 
-Route::view('/', 'home')->name('home');
+Route::middleware('cache.headers:public;max_age=2628000;etag')->group(function () {
+    Route::view('/', 'home')->name('home');
+    Route::view('/speaking', 'speaking')->name('speaking');
+});
 
-Route::view('/speaking', 'speaking')->name('speaking');
 Route::redirect('/presents', '/speaking');
 Route::redirect('/slides', '/speaking');
 Route::redirect('/presentations', '/speaking');
 Route::redirect('/presented', '/speaking');
 
 Route::redirect('/found', 'https://found.jkudish.com');
-
