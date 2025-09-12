8bd7aa0517fde8bf6977050b4f94a25e66429433
Joey Kudish <joachim.kudish@gmail.com>
2025-09-12 21:29:25 +0700

    fiix url in sitemap

--- tests/Feature/SitemapTest.php
+++ tests/Feature/SitemapTest.php
@@ -13,12 +13,12 @@ it('contains all main pages in sitemap', function () {
 
     $response->assertOk()
         ->assertSee('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"', false)
-        ->assertSee('jkudish.test</loc>', false)
-        ->assertSee('jkudish.test/speaking</loc>', false)
-        ->assertSee('jkudish.test/services</loc>', false)
-        ->assertSee('jkudish.test/projects</loc>', false)
-        ->assertSee('jkudish.test/newsletter</loc>', false)
-        ->assertSee('jkudish.test/contact</loc>', false);
+        ->assertSee('jkudish.com</loc>', false)
+        ->assertSee('jkudish.com/speaking</loc>', false)
+        ->assertSee('jkudish.com/services</loc>', false)
+        ->assertSee('jkudish.com/projects</loc>', false)
+        ->assertSee('jkudish.com/newsletter</loc>', false)
+        ->assertSee('jkudish.com/contact</loc>', false);
 });
 
 it('includes priority tags for pages', function () {
