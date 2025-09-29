c5dab4e8ba5c0daf7ac9b170422e448612183519
Joey Kudish <joachim.kudish@gmail.com>
2025-09-29 18:46:25 +0700

    exclude newsletter from 419

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\TrackPageViewMiddleware::class,
         ]);
+
+        $middleware->validateCsrfTokens(except: [
+            'newsletter',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
